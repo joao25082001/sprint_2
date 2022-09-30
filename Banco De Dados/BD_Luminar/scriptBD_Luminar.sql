f8465312af6415d9daab4ad45c998ee2b2c4658e
benichel <giovanna.santos@sptech.school>
2022-09-30 00:04:49 -0300

    create table das tabelas /funcionario/ambiente/sensor

--- Banco De Dados/BD_Luminar/scriptBD_Luminar.sql
+++ Banco De Dados/BD_Luminar/scriptBD_Luminar.sql
@@ -22,16 +22,40 @@ CREATE TABLE endereco(
     fkEmpresa INT, FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
--- Tabela Funcionario
 
+CREATE TABLE funcionario(
+	idFuncionario INT auto_increment,
+	cpf VARCHAR (11),
+	nome VARCHAR (45),
+	usuario VARCHAR (45),
+	senha VARCHAR (45),
+	setor VARCHAR (45),
+	fkEmpresa INT,
+	FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
+	PRIMARY KEY (idFuncionario, fkEmpresa)
+); 
 -- Tabela Local
+CREATE TABLE ambiente(
+	idAmbiente INT PRIMARY KEY auto_increment, 
+	metros_quadrados INT,
+	qtd_sensor INT,
+	fkFuncionario INT, 
+	FOREIGN KEY (fkFuncionario) REFERENCES funcionario (idFuncionario)
+);
 
 -- Tabela sensor
+CREATE TABLE sensor(
+	idSensor INT PRIMARY KEY auto_increment,
+	nome_sensor VARCHAR (45),
+	fkAmbiente INT,
+	FOREIGN KEY (fkAmbiente) REFERENCES ambiente (idAmbiente)
+);
 
 CREATE TABLE registro(
 	idRegistro INT auto_increment,
     valor_luminosidade DECIMAL(4,2),
     data_hora DATETIME,
-    fkSensor INT, FOREIGN KEY (fkSensor) REFERENCES Sensor(idSensor),
+    fkSensor INT, FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor),
     PRIMARY KEY (idRegistro, fkSensor)
 );
+
